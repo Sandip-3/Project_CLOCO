bbc19308d77007311f657e78465db1360add7c10
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-26 16:20:20 +0545

    Routes : User Route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import healthRouter from "./Health/health.routes";
+import userRouter from "./User/user.routes";
 
 const router = Router();
 
 router.use("/health", healthRouter);
+router.use("/user", userRouter);
 
 export default router;
