d628821d941720cafcf1ec1113c9771341c3e2f9
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-26 22:49:33 +0545

    User: Get Users Route

--- server/src/routes/User/user.routes.ts
+++ server/src/routes/User/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import userController from "./user.controller";
 import { validateUserRigistration } from "../../middleware/validator";
+import { validateRole } from "../../middleware/validate.role";
 
 const userRouter = Router();
 
@@ -10,5 +11,5 @@ userRouter.post(
   userController.createUser
 );
 userRouter.get("/:userId", userController.getUser);
-
+userRouter.get("/", validateRole(["superadmin"]), userController.getUsers);
 export default userRouter;
