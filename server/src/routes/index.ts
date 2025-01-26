25bcbb9370ea344969e0127f0b1f403ca3747921
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-26 21:50:40 +0545

    Routes: Auth  Routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import healthRouter from "./Health/health.routes";
 import userRouter from "./User/user.routes";
+import authRouter from "./Auth/auth.routes";
 
 const router = Router();
 
+router.use('/', authRouter)
 router.use("/health", healthRouter);
 router.use("/user", userRouter);
 
