dca08340b76b1040ab028a0f1e4f353bc56b3ffe
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-27 16:49:34 +0545

    Routes: Artist Route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import healthRouter from "./Health/health.routes";
 import userRouter from "./User/user.routes";
 import authRouter from "./Auth/auth.routes";
+import artistRouter from "./Artist/artist.routes";
 
 const router = Router();
 
-router.use('/', authRouter)
+router.use("/", authRouter);
 router.use("/health", healthRouter);
 router.use("/user", userRouter);
+router.use("/artist", artistRouter);
 
 export default router;
