ca1137d8e4523bcd07adb50cfd7a1de4bc3ae8fd
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-27 21:30:39 +0545

    Artist: Get Artists Controller

--- server/src/routes/Artist/artist.controller.ts
+++ server/src/routes/Artist/artist.controller.ts
@@ -3,34 +3,47 @@ import { successResponse } from "../../utils/HttpResponse";
 import artistService from ".";
 import { Message } from "../../utils/Messages";
 
-
 const artistController = {
-    createArtist: async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const {
-              name,
-              dob,
-              gender,
-              address,
-              first_release_year,
-              no_of_albums_released,
-            } = req.body;
-            const artist = await artistService.createArtist({
-              name,
-              dob,
-              gender,
-              address,
-              first_release_year,
-              no_of_albums_released,
-            });
-            return successResponse({
-                response: res,
-                message: Message.artist.artist_register_success,
-                data: artist,
-            })
-        } catch (error) {
-            next(error);
-        }
+  createArtist: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const {
+        name,
+        dob,
+        gender,
+        address,
+        first_release_year,
+        no_of_albums_released,
+      } = req.body;
+      const artist = await artistService.createArtist({
+        name,
+        dob,
+        gender,
+        address,
+        first_release_year,
+        no_of_albums_released,
+      });
+      return successResponse({
+        response: res,
+        message: Message.artist.artist_register_success,
+        data: artist,
+      });
+    } catch (error) {
+      next(error);
     }
-}
+  },
+  getArtists: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const page = parseInt(req.query.page as string) || 1;
+      const limit = parseInt(req.query.limit as string) || 10;
+      const artists = await artistService.getArtists(page, limit);
+      return successResponse({
+        response: res,
+        message: Message.artist.artist_fetch_success,
+        data: artists,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
 export default artistController;
