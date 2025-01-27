6d07b61ea6236056b36e8a5b2c20690953cd6660
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-27 09:47:24 +0545

    User: Update Profile Controller

--- server/src/routes/User/user.controller.ts
+++ server/src/routes/User/user.controller.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { successResponse } from "../../utils/HttpResponse";
 import userServices from ".";
 import { Message } from "../../utils/Messages";
+import authHelper from "../Auth/auth.helper";
+import CustomError from "../../utils/Error";
 
 const userController = {
   createUser: async (req: Request, res: Response, next: NextFunction) => {
@@ -35,37 +37,76 @@ const userController = {
     } catch (error) {
       next(error);
     }
-    },
-    getUser: async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const { userId } = req.params;
-            const user = await userServices.getUser(Number(userId));
-            return successResponse({
-                response: res,
-                message: Message.user.user_found,
-                data: user,
-            })
-        } catch (error) {
-            next(error);
-        }
-  }, 
-    getUsers : async (req: Request, res: Response, next: NextFunction) => {
-      try {
-          const page = req.query.page
-            ? parseInt(req.query.page as string, 10)
-            : 1;
-          const limit = req.query.limit
-            ? parseInt(req.query.limit as string, 10)
-            : 10;
-            const users = await userServices.getUsers(page , limit);
-            return successResponse({
-                response: res,
-                message: Message.user.users_found,
-                data: users,
-            })
-        } catch (error) {
-            next(error);
-        }
+  },
+  getUser: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userId } = req.params;
+      const user = await userServices.getUser(Number(userId));
+      return successResponse({
+        response: res,
+        message: Message.user.user_found,
+        data: user,
+      });
+    } catch (error) {
+      next(error);
     }
+  },
+  getUsers: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const page = req.query.page ? parseInt(req.query.page as string, 10) : 1;
+      const limit = req.query.limit
+        ? parseInt(req.query.limit as string, 10)
+        : 10;
+      const users = await userServices.getUsers(page, limit);
+      return successResponse({
+        response: res,
+        message: Message.user.users_found,
+        data: users,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  updateUserProfile: async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const id = Number(res.locals.user.id);
+      const userId = parseInt(req.params.userId);
+      // console.log(id);
+      // console.log(userId);
+      if (res.locals.user.role != "superadmin" && id !== Number(userId)) {
+        throw new CustomError("Unauthorized user", 401);
+      }
+      // Extract user details from request body
+      const { first_name, last_name, dob, phone, address, email, password } =
+        req.body;
+
+      // Prepare updated user data
+      const updatedUserData: Record<string, any> = {};
+      if (first_name) updatedUserData.first_name = first_name;
+      if (email) updatedUserData.email = email;
+      if (last_name) updatedUserData.last_name = last_name;
+      if (phone) updatedUserData.phone = phone;
+      if (address) updatedUserData.address = address;
+      if (dob) updatedUserData.dob = dob;
+
+      if (password) updatedUserData.password = await authHelper.hash(password);
+      const updatedUser = await userServices.updateUserById(
+        userId,
+        updatedUserData
+      );
+      return successResponse({
+        response: res,
+        message: Message.user.user_update_success,
+        data: updatedUser,
+      });
+    } catch (error) {
+      console.error("Error updating user profile:", error);
+      next(error);
+    }
+  },
 };
 export default userController;
