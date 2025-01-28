af10c908adb5ef8bdb6fff6102da5556257a5a1f
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-28 14:31:02 +0545

    Music: Get Music By Artist

--- server/src/routes/Music/music.services.ts
+++ server/src/routes/Music/music.services.ts
@@ -26,21 +26,21 @@ class MusicServices {
       client.release();
     }
   }
-    async updateMusicById(id: number, updatedData: Record<string, any>) {
-        const client = await this.pool.connect();
-        // console.log(id);
-        if (!id || isNaN(id)) {
-          throw new Error("Invalid Music ID");
-        }
-        try {
-          //Checking if fields are modified
-          if (Object.keys(updatedData).length === 0) {
-            throw new Error("No valid fields provided for update");
-          }
-          console.log(updatedData);
-    
-          // SQL query for updates
-          const query = `
+  async updateMusicById(id: number, updatedData: Record<string, any>) {
+    const client = await this.pool.connect();
+    // console.log(id);
+    if (!id || isNaN(id)) {
+      throw new Error("Invalid Music ID");
+    }
+    try {
+      //Checking if fields are modified
+      if (Object.keys(updatedData).length === 0) {
+        throw new Error("No valid fields provided for update");
+      }
+      console.log(updatedData);
+
+      // SQL query for updates
+      const query = `
               UPDATE "music"
               SET ${Object.keys(updatedData)
                 .map((key, index) => `"${key}" = $${index + 1}`)
@@ -48,48 +48,75 @@ class MusicServices {
               WHERE id = $${Object.keys(updatedData).length + 1}
               RETURNING *;
             `;
-    
-          // Combining updatedData values with the music ID
-          const values = [...Object.values(updatedData), id];
-    
-          const result = await client.query(query, values);
-    
-          if (result.rowCount === 0) {
-            throw new CustomError(`Music not found`, 404);
-          }
-          const updatedMusic = result.rows[0];
-          const musicDetail = JSON.parse(JSON.stringify(updatedMusic));
-          delete musicDetail.password;
-          return musicDetail;
-        } catch (err: any) {
-          throw err;
-        } finally {
-          client.release();
-        }
+
+      // Combining updatedData values with the music ID
+      const values = [...Object.values(updatedData), id];
+
+      const result = await client.query(query, values);
+
+      if (result.rowCount === 0) {
+        throw new CustomError(`Music not found`, 404);
+      }
+      const updatedMusic = result.rows[0];
+      const musicDetail = JSON.parse(JSON.stringify(updatedMusic));
+      delete musicDetail.password;
+      return musicDetail;
+    } catch (err: any) {
+      throw err;
+    } finally {
+      client.release();
     }
-    async deleteMusicById(id: number) {
-        const client = await this.pool.connect();
-        if (!id || isNaN(id)) {
-          throw new Error("Invalid Music ID");
-        }
-        try {
-          const existingMusic = await client.query(
-            `SELECT * FROM "music" WHERE id = $1`,
-            [id]
-          );
-          if (existingMusic.rows.length === 0) {
-            throw new CustomError(`Music not found`, 404);
-          }
-          const result = await client.query(
-            `DELETE FROM "music" WHERE id = $1 RETURNING *`,
-            [id]
-          );
-          return result.rows[0];
-        } catch (error) {
-          throw error;
-        } finally {
-          client.release();
-        }
+  }
+  async deleteMusicById(id: number) {
+    const client = await this.pool.connect();
+    if (!id || isNaN(id)) {
+      throw new Error("Invalid Music ID");
     }
+    try {
+      const existingMusic = await client.query(
+        `SELECT * FROM "music" WHERE id = $1`,
+        [id]
+      );
+      if (existingMusic.rows.length === 0) {
+        throw new CustomError(`Music not found`, 404);
+      }
+      const result = await client.query(
+        `DELETE FROM "music" WHERE id = $1 RETURNING *`,
+        [id]
+      );
+      return result.rows[0];
+    } catch (error) {
+      throw error;
+    } finally {
+      client.release();
+    }
+  }
+  async getMusicByArtistId(artist_id: number, page: number, limit: number) {
+    const client = await this.pool.connect();
+    try {
+      const offset = (page - 1) * limit;
+      const result = await client.query(
+        `SELECT * FROM "music" WHERE artist_id = $1 LIMIT $2 OFFSET $3`,
+        [artist_id, limit, offset]
+      );
+      if (result.rows.length === 0) {
+        throw new CustomError(`Music not found`, 404);
+      }
+      const totalCount = await client.query(
+        `SELECT COUNT(*) FROM "music" WHERE artist_id = $1`,
+        [artist_id]
+      );
+      return {
+        data: result.rows,
+        total: parseInt(totalCount.rows[0].count),
+        page: page,
+        limit: limit,
+      };
+    } catch (error) {
+      throw error;
+    } finally {
+      client.release();
+    }
+  }
 }
 export default MusicServices;
