5bcd487fbed8f03eff9fc7f7c253d76a1d610883
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-29 09:50:14 +0545

    React: Vite with Shadcn Initial Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+      <Button className="m-12">Hello</Button>
+    </div>
+  )
+}
+
+export default App
