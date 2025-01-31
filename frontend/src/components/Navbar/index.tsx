f1b73c9193959c3a9a94a86192def2bab74aafbe
Sandip-3 <sandeeppoudel796@gmail.com>
2025-01-31 09:12:58 +0545

    Navbar Updated

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useUser } from "@/context/User";
 
 const index = () => {
+  const { user, setUser } = useUser();
+  const handleLogout = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    localStorage.removeItem("user");
+    setUser(null);
+  };
   return (
     <div className="top-0 sticky z-10 bg-slate-100 p-2 w-screen flex justify-between items-center">
       <div className="appName p-2">
@@ -8,8 +15,22 @@ const index = () => {
       </div>
       <div>
         <ul className="flex space-x-12 p-2">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          <li>
+            {user ? (
+              <Link to="/login">{user.name}</Link>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+          </li>
+          <li>
+            {user ? (
+              <Link to="/logout" onClick={(event) => handleLogout(event)}>
+                Logout
+              </Link>
+            ) : (
+              <Link to="/signup">Signup</Link>
+            )}
+          </li>
         </ul>
       </div>
     </div>
