c4fe7128ac7c96fba55f62faefb52590677a7130
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-01 09:12:51 +0545

    Added User Login and Register Api

--- frontend/src/api/user.api.ts
+++ frontend/src/api/user.api.ts
@@ -3,3 +3,11 @@ import { appAxios } from "@/config/axios";
 export const loginAdmin = (values: any) => {
   return appAxios.post("/admin/login", values);
 };
+
+export const registerUser = (values: any) => {
+  return appAxios.post(`/user/register`, values);
+};
+
+export const loginUser = (values: any) => {
+  return appAxios.post(`/user/login`, values);
+}
