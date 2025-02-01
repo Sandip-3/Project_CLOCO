103535fc96a042feec1e8cf78fccb50d95d90978
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-01 09:12:12 +0545

    Handle Logout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import { useUser } from "@/context/User";
+import { toast } from "sonner";
 
 const index = () => {
   const { user, setUser } = useUser();
   const handleLogout = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
-    localStorage.removeItem("user");
+    localStorage.removeItem("userData");
+    localStorage.removeItem("accessToken");
     setUser(null);
+    toast.success("Logged out successfully");
   };
   return (
     <div className="top-0 sticky z-10 bg-slate-100 p-2 w-screen flex justify-between items-center">
@@ -15,22 +18,18 @@ const index = () => {
       </div>
       <div>
         <ul className="flex space-x-12 p-2">
-          <li>
-            {user ? (
-              <Link to="/login">{user.name}</Link>
-            ) : (
-              <Link to="/login">Login</Link>
-            )}
-          </li>
-          <li>
+          <div>
+            {user ? <li>{user.name}</li> : <Link to="/user/login">Login</Link>}
+          </div>
+          <div>
             {user ? (
               <Link to="/logout" onClick={(event) => handleLogout(event)}>
                 Logout
               </Link>
             ) : (
-              <Link to="/signup">Signup</Link>
+              <Link to="/user/register">Signup</Link>
             )}
-          </li>
+          </div>
         </ul>
       </div>
     </div>
