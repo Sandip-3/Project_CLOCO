6a82873bd476daf4f4bf09709e6180d54c6d38d4
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-01 14:06:14 +0545

    Fixed Logout and Other Redirect

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
 import { useUser } from "@/context/User";
 import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 const index = () => {
   const { user, setUser } = useUser();
+  const navigate = useNavigate();
   const handleLogout = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     localStorage.removeItem("userData");
     localStorage.removeItem("accessToken");
     setUser(null);
     toast.success("Logged out successfully");
+     setTimeout(() => {
+       navigate("/user/login");
+     }, 100);
   };
   return (
     <div className="top-0 sticky z-10 bg-slate-100 p-2 w-screen flex justify-between items-center">
@@ -27,7 +32,7 @@ const index = () => {
                 Logout
               </Link>
             ) : (
-              <Link to="/user/register">Signup</Link>
+              <Link to="/">Signup</Link>
             )}
           </div>
         </ul>
