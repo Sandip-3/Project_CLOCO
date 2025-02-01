8de61226ef2fd7a1cafdc5df8f42c2cb9b2138d1
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-01 17:01:14 +0545

    Update Artist Api

--- frontend/src/api/artist.api.ts
+++ frontend/src/api/artist.api.ts
@@ -11,3 +11,11 @@ export const registerArtist = (values: any) => {
 export const deleteArtistById = (id: number) => {
   return appAxios.delete(`/artist/${id}`);
 }
+
+export const getArtistById = (id: number) => {
+  return appAxios.get(`/artist/${id}`);
+}
+
+export const updateArtist = (id: number, values: any) => {
+  return appAxios.patch(`/artist/${id}`, values);
+}
