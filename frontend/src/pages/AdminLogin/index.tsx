a92862415906f62b19b6d45bb7356392192658a3
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-01 09:11:15 +0545

    Handled Error

--- frontend/src/pages/AdminLogin/index.tsx
+++ frontend/src/pages/AdminLogin/index.tsx
@@ -5,7 +5,8 @@ import { useNavigate } from "react-router-dom";
 import { loginAdmin } from "@/api/user.api";
 import { useUser } from "@/context/User";
 import { useEffect } from "react";
-import { UserLogin } from "@/Types";
+import { AdminLoginData } from "@/Types";
+import { toast } from "sonner";
 
 const AdminLogin = () => {
   const { setUser } = useUser();
@@ -26,40 +27,44 @@ const AdminLogin = () => {
     // }
   }, []);
 
-  const onSubmit = async (values: UserLogin) => {
+  const onSubmit = async (values: AdminLoginData) => {
     await loginAdmin(values)
       .then((response) => {
         const data = response.data;
-        // localStorage.setItem("userData", JSON.stringify(data.data.userInfo));
-        // localStorage.setItem(
-        //   "accessToken",
-        //   JSON.stringify(data.data.accessToken)
-        // );
+        localStorage.setItem("userData", JSON.stringify(data.data.userInfo));
+        localStorage.setItem(
+          "accessToken",
+          JSON.stringify(data.data.accessToken)
+        );
         console.log(data.data);
-        setUser(data.data.user)
-        // setUser(data.data.userInfo);
-        // console.log(data.data.userInfo);
+        setUser(data.data.user);
+
         const message = response?.data.message;
-        console.log(message)
-        // navigate("/home");
+        toast.success(message);
+        navigate("/home");
       })
       .catch((error) => {
         const data = error.response?.data;
+        if (Array.isArray(data?.data) && data.data.length > 0) {
+          toast.error(data.data[0].msg);
+        } else {
+          toast.error(data?.message || "An unknown error occurred");
+        }
       });
   };
 
   return (
     <div className="w-screen h-[90vh] p-4  flex flex-col md:flex-row  md:items-center md:justify-center">
       <div className="signup_form width-full md:w-1/4">
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 border border-gray-50 p-4 shadow-lg rounded-lg">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 border border-gray-50 p-4 shadow-lg rounded-lg"
+        >
           <p className="text-3xl font-serif text-center">Login</p>
           <div className="space-y-6">
             <div>
               <label className="block mb-1">Email</label>
-              <Input
-                placeholder="Email"
-                {...form.register("email")}
-              />
+              <Input placeholder="Email" {...form.register("email")} />
             </div>
             <div>
               <label className="block mb-1">Password</label>
