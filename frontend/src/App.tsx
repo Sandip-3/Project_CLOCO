23f5787124e012c64fdb8450ce05490299fb91fc
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-01 14:01:54 +0545

    NPM Package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,11 @@ import AdminLogin from "./pages/AdminLogin";
 import { Toaster } from "./components/ui/sonner";
 import Register from "./pages/Register";
 import UserLogin from "./pages/UserLogin";
+import ProtectedRoute from "./components/Protected";
+import AdminDashboard from "./pages/AdminDashboard";
+import Home from "./pages/Home";
+import Unauthorized from "./pages/Unauthorized";
+import ProtectedAdminRoute from "./components/AdminProtected/AdminRoute";
 
 function App() {
   return (
@@ -11,8 +16,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/user/register" element={<Register />} />
+          <Route index element={<Register />} />
           <Route path="/user/login" element={<UserLogin />} />
+          <Route path="/unauthorized" element={<Unauthorized />} />
+          {/* Protected User Dashboard */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
+
+          {/* Protected Admin Dashboard */}
+          <Route element={<ProtectedAdminRoute />}>
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          </Route>
         </Route>
       </Routes>
       <Toaster richColors toastOptions={{}} closeButton theme="light" />
