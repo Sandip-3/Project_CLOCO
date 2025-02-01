76701ff0858847b0e5376193c7dd250e206bcf0d
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-01 09:14:11 +0545

    Added User Login and Register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import AdminLogin from "./pages/AdminLogin";
+import { Toaster } from "./components/ui/sonner";
+import Register from "./pages/Register";
+import UserLogin from "./pages/UserLogin";
 
 function App() {
   return (
@@ -8,8 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/user/register" element={<Register />} />
+          <Route path="/user/login" element={<UserLogin />} />
         </Route>
       </Routes>
+      <Toaster richColors toastOptions={{}} closeButton theme="light" />
     </>
   );
 }
