39034f70b99bf50027c43e1388991a052e52408e
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-02 18:27:01 +0545

    Create Music Component

---
+++ frontend/src/components/CreateMusic/index.tsx
@@ -0,0 +1,192 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "sonner";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { createMusic } from "@/api/music.api";
+
+const formSchema = z.object({
+  title: z.string().trim().min(2, {
+    message: "Album Title must be at least 2 characters.",
+  }),
+  album_name: z.string().trim().min(2, {
+    message: "Album Name must be at least 2 characters.",
+  }),
+  genre: z.enum(["classic", "rock", "jazz", "country", "rnb"], {
+    message: "Select a valid genre",
+  }),
+  release_year: z.string().trim().min(4, {
+    message: "Release Year must be a valid year.",
+  }),
+  no_of_tracks: z.string().trim().min(1, {
+    message: "Number of tracks must be at least 1.",
+  }),
+});
+
+interface MusicAlbumRegisterProps {
+  artistId: string;
+}
+
+const index: React.FC<MusicAlbumRegisterProps> = ({
+  artistId,
+}) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      album_name: "",
+      genre: "classic",
+      release_year: "",
+      no_of_tracks: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const formData = new FormData();
+    formData.append("artist_id", artistId);
+    formData.append("title", values.title);
+    formData.append("album_name", values.album_name);
+    formData.append("genre", values.genre);
+    formData.append("release_year", String(values.release_year));
+    formData.append("no_of_tracks", String(Number(values.no_of_tracks)));
+
+    try {
+      await createMusic(Object.fromEntries(formData));
+      toast.success("Music album registration successful!");
+      form.reset();
+    } catch (error: any) {
+      const data = error.response?.data.data[0].msg;
+      toast.error(data);
+    }
+  };
+
+  return (
+    <div className="w-auto h-auto mt-[10%] flex justify-center items-center">
+      <div className="w-full max-w-md border bg-white border-gray-300 shadow-lg rounded-lg p-4">
+        <Form {...form}>
+          <p className="text-3xl font-serif text-center">
+            Register Music Album
+          </p>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Album Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Album Title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="album_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Album Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Album Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="genre"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Genre</FormLabel>
+                  <RadioGroup
+                    defaultValue="classic"
+                    className="flex gap-2"
+                    onValueChange={field.onChange}
+                  >
+                    <RadioGroupItem value="classic" id="classic" />
+                    <label htmlFor="classic" className="ml-1 text-sm">
+                      Classic
+                    </label>
+                    <RadioGroupItem value="rock" id="rock" />
+                    <label htmlFor="rock" className="ml-1 text-sm">
+                      Rock
+                    </label>
+                    <RadioGroupItem value="jazz" id="jazz" />
+                    <label htmlFor="jazz" className="ml-1 text-sm">
+                      Jazz
+                    </label>
+                    <RadioGroupItem value="country" id="country" />
+                    <label htmlFor="country" className="ml-1 text-sm">
+                      Country
+                    </label>
+                    <RadioGroupItem value="rnb" id="rnb" />
+                    <label htmlFor="rnb" className="ml-1 text-sm">
+                      R&B
+                    </label>
+                  </RadioGroup>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="release_year"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Release Year</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Release Year"
+                      type="number"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="no_of_tracks"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Number of Tracks</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Number of Tracks"
+                      type="number"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="w-full" type="submit">
+              Register Album
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default index;
