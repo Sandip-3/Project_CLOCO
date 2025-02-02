5b87cf9534cc291cf7cff10e367cd46bf0251f7e
Sandip-3 <sandeeppoudel796@gmail.com>
2025-02-02 08:42:09 +0545

    Added User API Endpoints

--- frontend/src/api/user.api.ts
+++ frontend/src/api/user.api.ts
@@ -11,3 +11,19 @@ export const registerUser = (values: any) => {
 export const loginUser = (values: any) => {
   return appAxios.post(`/user/login`, values);
 }
+
+export const getUsers = () => {
+  return appAxios.get("/user");
+}
+
+export const deleteUserById = (id: number) => {
+  return appAxios.delete(`/user/${id}`);
+}
+
+export const getUserById = (id: number) => {
+  return appAxios.get(`/user/${id}`);
+}
+
+export const updateUser = (id: number, values: any) => {
+  return appAxios.patch(`/user/${id}`, values);
+}
